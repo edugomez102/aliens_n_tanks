90117e40eed4ef0b759ebf88bbb2749c9804989d
mpereztarruella <mpt36@gcloud.ua.es>
2022-11-02 18:18:14 +0100

    Trackeados los cambios de la rama new_prot

--- src/assets/compress/screenmenu.h
+++ src/assets/compress/screenmenu.h
@@ -2,22 +2,22 @@
 // Compresor used:   zx7b
 // Files compressed: [ 'src/assets/sprites/space_avocados_title.bin' ]
 // Uncompressed:     16384 bytes
-// Compressed:       1514 bytes
-// Space saved:      14870 bytes
+// Compressed:       1966 bytes
+// Space saved:      14418 bytes
 //
 
-#ifndef screenmenu_1514_H
-#define screenmenu_1514_H
+#ifndef screenmenu_1966_H
+#define screenmenu_1966_H
 
 
 // Declaration of the compressed array
-extern const unsigned char screenmenu[1514];
+extern const unsigned char screenmenu[1966];
 
 // Address of the latest byte of the compressed array (for unpacking purposes)
-#define screenmenu_end      (screenmenu + 1514 - 1)
+#define screenmenu_end      (screenmenu + 1966 - 1)
 
 // Compressed and uncompressed sizes
-#define screenmenu_size_z   1514
+#define screenmenu_size_z   1966
 #define screenmenu_size     16384
 
 
